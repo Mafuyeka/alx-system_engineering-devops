03b3509023a45d6af957cea3a4417a0a50c647c8
Mafuyeka <tshepisoroshina@gmail.com>
2024-01-31 10:13:50 -0800

    Updated

--- 0x06-regular_expressions/5-beginning_and_end.rb
+++ 0x06-regular_expressions/5-beginning_and_end.rb
@@ -4,4 +4,4 @@
 # The regular expression must be exactly matching a string that starts,
 # with h ends with n and can have any single character in between
 
-puts ARGV[0].scan(/^h.n$/).join1
+puts ARGV[0].scan(/^h.n$/).join
