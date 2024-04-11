e950a09585c6762b9d50f97ccfea807a3c334907
Mafuyeka <tshepisoroshina@gmail.com>
2024-04-11 09:43:11 -0700

    Updated

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,9 @@
-# install puppet-lint -v 2.5.0
+# create_school_file.pp
 
-exec { 'puppet-lint':
-  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
+file { '/tmp/school':
+  ensure  => file,                  # Ensure it's a file
+  mode    => '0744',                # Set file permissions to 0744
+  owner   => 'www-data',            # Set file owner to www-data
+  group   => 'www-data',            # Set file group to www-data
+  content => "I love Puppet\n",     # Set the content of the file
 }
